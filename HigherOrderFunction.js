e30a2a7538145b16e20da9b55658ad1bd86303c5
PavankumarAN <pavankumaran51@gmail.com>
2018-10-26 22:45:05 +0530

    High

---
+++ HigherOrderFunction.js
@@ -0,0 +1,8 @@
+//setTimeout(function(){alert("Full-stack Web - Developer within this December")},4000);
+/*function aim() {
+    alert("Full-stack Web-developer within this december");
+}*/
+//setTimeout(aim,4000);
+/* setInterval(function(){
+    alert("Full-Stack Web-Developer within this December");
+},2000); */
