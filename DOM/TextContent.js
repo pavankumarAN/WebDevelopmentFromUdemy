ac2964d8c3e0a6b0299a87ef25a6af5e59ce3415
PavankumarAN <pavankumaran51@gmail.com>
2018-11-04 18:51:17 +0530

    DOM

---
+++ DOM/TextContent.js
@@ -0,0 +1,6 @@
+const  item = document.getElementById("special");
+console.log(item);
+console.log(item.nodeValue);
+console.log(item.firstChild.nodeValue);//instead you can use
+console.log(item.textContent);
+
