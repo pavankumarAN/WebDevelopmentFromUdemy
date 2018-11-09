a42a9a8f82e665ae50bb7e263af7efec4f3ed920
PavankumarAN <pavankumaran51@gmail.com>
2018-11-09 22:58:17 +0530

    DOM

---
+++ DOM/ChangeCSS.js
@@ -0,0 +1,7 @@
+const item =  document.getElementById("item");
+/* item.style.backgroundColor = "red";
+item.style.color = "white";
+item.style.fontSize = "3rem";
+item.style.fontStyle = "italic  "; */
+item.classList.add('special','hide');
+item.classList.remove('hide');
